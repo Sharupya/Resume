1a55d900364a182984dd92cde7dce838c9e44e19
Sharupya Barua <sharupyabarua1427@gmail.com>
2021-07-04 21:35:56 +0600

    gffg

---
+++ jmc.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <h1> Bandarban </h1>
+    <link rel="stylesheet" href ="style.css">
+</head>
+<body>
+<p>  asafgndsnfhgfnbcbnvcbncnbcngxngfxnfxnfxfnxmfxmhfxmfxmhfxmbxmbxmfc </p>
+
+
+<ol>
+<b>
+    <li> 1.shangu</li>
+    <li>2.nafakhum</li>
+    <li>3.amiakhum</li>
+   </b>  
+     </ol>
+     <div class= "jmc">
+     <h2>SHANGU</h2>
+     <p id ="s">1.shangu   : kjsgfdsjkvk jdxf hbkjvxdk jfvb kjfxb jkcgkjnb kjcgnbkjj</p>
+     </div>
+     
+     <div class= "jmc">
+     <h2>nafakhum</h2>
+     <p id ="n"> 2.nafakhum: jshfkjak jegkjsdhg kjhsdkjgksdjgk jdfkjgkd jfhgkjhdkfj</p>
+     </div> 
+
+     <div class= "jmc">
+     <h2>amiakhum</h2>
+     <p id ="a">3.amiakhum :skjhg kjdhg kjhdfkjgkjdfhk gjhdfk jgkjfgj fjdkb lkjfgbjkl fjj</p>     
+     </div>
+     
+     
+</body>
+</html>
---
+++ power.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Document</title>
+    <style>
+    #obama{
+        color: darkorange;
+    }
+    .name{
+        color: darkgoldenrod;
+    }
+          h1{
+              background-color: cyan;
+          }
+
+    </style>
+</head>
+<body>
+    <h1 style="color:darkmagenta;" id ="obama" class= "name">faddgsgd</h1>
+</body>
+</html>
